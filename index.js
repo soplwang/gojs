8925577b8760964f749a60b7b4acb42c64e75600
Sopl'Wang <sopl.wang@gmail.com>
2015-03-04 10:05:50 +0800

    fix example again

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ exports.then = then;
 // Ref: http://swannodette.github.io/2013/08/24/es6-generators-and-csp/
 //
 // go(function* (chan) {
-//   var ch1 = Channel();
 //   var ch2 = Channel();
 //
 //   var r1 = yield db.query('SELECT 1', chan);
 //   var r2 = yield request('http://www.google.com', chan);
 //
-//   db1.query('SELECT 1 FROM dummy', then(ch1, function (res) { ch1(null, res[0]); }));
+//   db1.query('SELECT 1 FROM dummy', then(ch2, function (res) { ch2(null, res[0]); }));
 //   db2.query('SELECT 3', chan);
-//   var r3 = yield ch1;
+//   var r3 = yield ch2;
 //   var r4 = yield;
 //
 //   go(function* (ch3) {
@@ -24,9 +23,12 @@ exports.then = then;
 //     db2.query('SELECT 3', bind(ch3, 'r6'));
 //     var rx = yield;
 //     var ry = yield;
-//     chan(null, rx[0] + ry[0]);
+//     chan(null, rx[1] + ry[1]);
 //   });
-//   yield;
+//
+//   try {
+//     var r5 = yield;
+//   } catch (e) {}
 //
 //   redis.get('k1', chan);
 //   redis.hget('k2', chan);
