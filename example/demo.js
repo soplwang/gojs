35dca100750cc9a8300d81fe0febcabae7f81159
Sopl'Wang <sopl.wang@gmail.com>
2015-04-18 20:38:26 +0800

    Improve example to reduce confuse usages

--- example/demo.js
+++ example/demo.js
@@ -8,33 +8,33 @@ var then = require('../').then;
 // Ref: http://swannodette.github.io/2013/08/24/es6-generators-and-csp/
 //
 go(function* (chan) {
-  var ch2 = Channel();
-
   db.query('SELECT 1', chan);
   var r1 = yield chan;
 
   request('http://www.google.com', chan);
   var r2 = yield;
 
-  db.query('SELECT 1 FROM dummy', then(ch2, function (res) { ch2(null, res[0]); }));
-  db.query('SELECT 3', chan);
-  var r3 = yield ch2;
-  var r4 = yield;
+  redis.get('k1', chan);
+  redis.hget('k2', chan);
+  var rk1 = yield;
+  var rk2 = yield;
 
-  go(function* (ch3) {
-    db.query('SELECT 1', bind(ch3, 'r5'));
-    db.query('SELECT 3', bind(ch3, 'r6'));
+  go(function* (ch2) {
+    db.query('SELECT 1', bind(ch2, 'r3'));
+    db2.query('SELECT 3', bind(ch2, 'r4'));
     var rx = yield;
     var ry = yield;
     chan(null, rx[1] + ry[1]);
   });
 
   try {
-    var r7 = yield;
+    var rz = yield;
   } catch (e) {}
 
-  redis.get('k1', chan);
-  redis.hget('k2', chan);
-  var rk1 = yield;
-  var rk2 = yield;
+  var ch3 = Channel();
+
+  db.query('SELECT 1 FROM dummy', then(ch3, function (res) { ch3(null, res[0]); }));
+  db2.query('SELECT 3', chan);
+  var r3 = yield ch3;
+  var r4 = yield;
 });
